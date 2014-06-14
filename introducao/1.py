e1be32ed0a7968dac6bcd87a1cfb04318298339d
Leandro <leandrovamk@gmail.com>
2014-06-14 09:29:39 -0300

    Exercício 1

---
+++ introducao/1.py
@@ -0,0 +1,17 @@
+'''
+Created on 07/06/2014
+
+@author: leandroveigamichalczuk
+'''
+# imprime as notas e a media
+
+nome = raw_input ("Qual e seu nome\n\n")
+nota1 = float(raw_input("digite a nota 1\n\n"))
+nota2 = float(raw_input("digite a nota 2\n\n"))
+nota3 = float(raw_input("digite a nota 3\n\n"))
+nota4 = float(raw_input("digite a nota 4\n\n"))
+media = (nota1+nota2+nota3+nota4)/4
+
+
+
+print nome , "suas notas sao:\n\n", nota1, nota2, nota3, nota4, "\n\ne sua media final e:", media
